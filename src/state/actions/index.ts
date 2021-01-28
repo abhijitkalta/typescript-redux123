cb5255680eb30599e9554d1c4554f5e2611282d2
Abhijit Kalta <abhijitkalta@gmail.com>
2021-01-29 01:13:18 +0530

    Modify repositoriesReducer

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -1,6 +1,9 @@
+import { ActionType } from "../action-types/index";
+
 interface SearchRepositoriesAction {
   type: ActionType.SEARCH_REPOSITORIES;
 }
+
 interface SearchRepositoriesSuccessAction {
   type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
   payload: string[];
